5cc8c6758059701050d0a79980e3813e07a62ebd
D1MOND <D1MOND@REVISION-PC>
2022-05-03 15:05:21 +1000

    Добавьте файлы проекта.

---
+++ D1MOND_bot/Program.cs
@@ -0,0 +1,80 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Extensions.Polling;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+
+namespace D1MOND_bot
+{
+    struct BotUpdate
+    {
+        public string text;
+        public long id;
+        public string? username;
+    }
+    class Program
+    {
+        static TelegramBotClient Bot = new TelegramBotClient("5282240497:AAEr5Ud-2T0ksNGX_jLKt6qlGIWCXxeoNWE");
+
+        static string fileName = "updates.json";
+        static List<BotUpdate> botUpdates = new List<BotUpdate>();
+        static void Main(string[] args)
+        {
+            
+            try
+            {
+                var botUpdatesString = System.IO.File.ReadAllText(fileName);
+
+                botUpdates = JsonConvert.DeserializeObject<List<BotUpdate>>(botUpdatesString) ?? botUpdates;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error reading or deserializing {ex}");
+            }
+
+            var receiverOptions = new ReceiverOptions
+            {
+                AllowedUpdates = new UpdateType[]
+                {
+                    UpdateType.Message,
+                    UpdateType.EditedMessage,
+                }
+            };
+
+            Bot.StartReceiving(UpdateHandler, ErrorHandler, receiverOptions);
+
+            Console.ReadLine();
+        }
+
+        private static Task ErrorHandler(ITelegramBotClient arg1, Exception arg2, CancellationToken arg3)
+        {
+                throw new NotImplementedException();
+        }
+
+        private static async Task UpdateHandler(ITelegramBotClient bot, Update update, CancellationToken arg3)
+        {
+            if (update.Type == UpdateType.Message)
+            {
+                if (update.Message.Type == MessageType.Text)
+                {
+                    var _botUpdate = new BotUpdate
+                    {
+                        text = update.Message.Text,
+                        id = update.Message.Chat.Id,
+                        username = update.Message.Chat.Username
+                    };
+
+                    botUpdates.Add(_botUpdate);
+
+                    var botUpdatesString = JsonConvert.SerializeObject(botUpdates);
+
+                    System.IO.File.WriteAllText(fileName, botUpdatesString);
+                }
+            }
+        }
+    }
+}
